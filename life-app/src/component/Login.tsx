e88608f37ce83b06c710434490a0456d699b6695
O-ladapo <olaniran.da06@gmail.com>
2026-08-23 11:14:07 +0100

    finish login page design

--- life-app/src/component/Login.tsx
+++ life-app/src/component/Login.tsx
@@ -9,18 +9,18 @@ function Login() {
             </div>
             <div className="right_section">
                 <div className="right_content">
-                    <div>
                       <h1>Login</h1>
                       <h2>Welcome back! Please sign in to continue.</h2>
-                    </div>
                 
                     <div className="input_section">
                         <input placeholder="Username" /> 
                         <input type="password" placeholder="Password" /> 
                     </div>
-                    <div className="login_button">
-                        <button type="submit">Login</button>
-                    </div>
+                    <button className="login_button" type="submit">Login</button>
+                    <h3>
+                        Don't have an account?{" "}
+                        <span className="sign_up_link">Sign Up</span>
+                    </h3>
                 </div>
             </div>
         
