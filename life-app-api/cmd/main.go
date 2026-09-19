722f763258f31cf883a40a75f3b1c1156f0388a2
O-ladapo <olaniran.da06@gmail.com>
2026-09-19 18:42:45 +0100

    add vercel link

--- life-app-api/cmd/main.go
+++ life-app-api/cmd/main.go
@@ -28,7 +28,7 @@ func main() {
 	router.SetTrustedProxies(nil)
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://life-app-blue-five.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
