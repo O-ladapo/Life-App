ce92a6c708e52194736bc0f544df74a270d5ed5a
O-ladapo <olaniran.da06@gmail.com>
2026-09-25 21:01:56 +0100

    Run migrations automatically on startup

--- life-app-api/cmd/main.go
+++ life-app-api/cmd/main.go
@@ -7,9 +7,24 @@ import (
 	"life_app_api/internal/handlers"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+func runMigrations(databaseURL string) error {
+	m, err := migrate.New("file://migrations", databaseURL)
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	cfg, err := config.Load()
@@ -23,6 +38,10 @@ func main() {
 	}
 
 	defer pool.Close()
+
+	if err := runMigrations(cfg.DatabaseURL); err != nil {
+		log.Fatal("Failed to run migrations: ", err)
+	}
 
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
